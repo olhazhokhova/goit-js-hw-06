891a530d08d7a8b6e0c93696b506f04e0cd87167
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-14 15:43:34 +0300

    add task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+function addIngredientsList(ingredients) {
+  const parentIngredients = document.querySelector('#ingredients');
+  
+  const ingredientsLi = ingredients.map(ingredient => {
+    const ingredientLi = document.createElement('li');
+    ingredientLi.textContent = ingredient;
+    ingredientLi.classList.add('item');
+    return ingredientLi;
+  });
+
+  parentIngredients.append(...ingredientsLi);
+  
+}
+
+addIngredientsList(ingredients);

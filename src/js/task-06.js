d93d27cf739cc7715d066a29db447e0f274edcf9
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-15 12:34:37 +0300

    add task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', () => {
+    input.classList = input.value.length >= input.dataset.length ? 'valid' : 'invalid';
+});

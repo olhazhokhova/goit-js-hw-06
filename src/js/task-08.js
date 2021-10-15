8518266648683c75cd245332b542d778cdfdc2c3
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-15 17:40:06 +0300

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const elem = event.currentTarget.elements;
+
+    if (elem.email.value !== '' && elem.password.value !== '') {
+        console.log({
+            email: elem.email.value,
+            password: elem.password.value
+        });
+        event.target.reset();
+    } else {
+        alert('Заполните все поля!');
+    }
+});

6ffa22c3a903546e6141425fb6a1de4f6dd3899c
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-16 14:04:55 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('.color');
+
+buttonRef.addEventListener('click', () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+});

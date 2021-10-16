62e85a5deb784c490d991888af0ca7da7829d33e
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-16 18:27:06 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
+const refs = {
+  create: document.querySelector('[data-create]'),
+  destroy: document.querySelector('[data-destroy]'),
+  input: document.querySelector('#controls input'),
+  boxesContainer: document.querySelector('#boxes'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  let listBoxes = '';
+  for (let i = 0; i < amount; i += 1) {
+    listBoxes += `<div style="width: ${10 * i + 30}px;height: ${
+      10 * i + 30
+    }px;background: ${getRandomHexColor()}"></div>`;
+  }
+  refs.boxesContainer.innerHTML = listBoxes;
+}
+
+function onClickCreate() {
+  refs.input.value && createBoxes(+refs.input.value);
+}
+
+function onClickDestroy() {
+  refs.input.value = '';
+  refs.boxesContainer.innerHTML = '';
+}
+
+refs.create.addEventListener('click', onClickCreate);
+refs.destroy.addEventListener('click', onClickDestroy);
